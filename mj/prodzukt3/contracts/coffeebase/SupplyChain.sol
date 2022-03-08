349f1867945c552db51b1e28d9cfd11b52f4036d
Marcin Jackowiak <marcin.jackowiak94@gmail.com>
2022-03-08 22:40:07 +0100

    Supply chain with ownable

--- mj/prodzukt3/contracts/coffeebase/SupplyChain.sol
+++ mj/prodzukt3/contracts/coffeebase/SupplyChain.sol
@@ -5,13 +5,10 @@ import "../coffeeaccesscontrol/ConsumerRole.sol";
 import "../coffeeaccesscontrol/DistributorRole.sol";
 import "../coffeeaccesscontrol/FarmerRole.sol";
 import "../coffeeaccesscontrol/RetailerRole.sol";
+import "../coffeecore/Ownable.sol";
 
 // Define a contract 'Supplychain'
-contract SupplyChain is ConsumerRole, DistributorRole, FarmerRole, RetailerRole {
-
-
-    // Define 'owner'
-    address owner;
+contract SupplyChain is ConsumerRole, DistributorRole, FarmerRole, RetailerRole, Ownable {
 
     // Define a variable called 'upc' for Universal Product Code (UPC)
     uint  upc;
@@ -71,10 +68,10 @@ contract SupplyChain is ConsumerRole, DistributorRole, FarmerRole, RetailerRole 
     event Purchased(uint upc);
 
     // Define a modifer that checks to see if msg.sender == owner of the contract
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
+    //    modifier onlyOwner() {
+    //        require(msg.sender == owner);
+    //        _;
+    //    }
 
     // Define a modifer that verifies the Caller
     modifier verifyCaller (address _address) {
@@ -149,15 +146,14 @@ contract SupplyChain is ConsumerRole, DistributorRole, FarmerRole, RetailerRole 
     // and set 'sku' to 1
     // and set 'upc' to 1
     constructor() payable {
-        owner = msg.sender;
         sku = 1;
         upc = 1;
     }
 
     // Define a function 'kill' if required
     function kill() public {
-        if (msg.sender == owner) {
-            selfdestruct(payable(owner));
+        if (msg.sender == owner()) {
+            selfdestruct(payable(owner()));
         }
     }
 
